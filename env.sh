029515448b39943e5786d046ad653d758cccef0c
rohollah903 <rohollah903@gmail.com>
2023-10-25 11:16:16 +0330

    Ready to train

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
-!gdown '1QiVLNjzdibU9R_fQPK3NnchX55UT4GHM'
+gdown '1QiVLNjzdibU9R_fQPK3NnchX55UT4GHM'
 
-!mkdir data
-!mv tickets.csv data
+mkdir data
+mv tickets.csv data
 
-!pip install rdt
+pip install rdt
